3de955cbd3630dbe6da03ca614c9353e082df1d0
qmei1123 <qmei@bu.edu>
2018-02-01 14:26:23 -0500

    add rows

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var num = [20];
+var yPosition = 150;
 function setup() {
     createCanvas(800, 800);
     background(50, 50, 200);
@@ -10,9 +11,13 @@ function draw() {
     strokeWeight = 10;
     stroke(255);
     for (var i = 0; i < 4; i++) {
-    ellipse(i*150 + 150, 100, num[0], num[0]);
+        ellipse(i*150 + 150, yPosition, num[0], num[0]);
         for (var x = 0; x < 100; x += 4) {
-            ellipse(i*150 + 150, 100, num[0]*x/2, num[0]*x/2);
+            ellipse(i*150 + 150, yPosition, num[0]*x/2, num[0]*x/2);
+            for (var y = 0; y < 4; y++) {
+                ellipse(i*150 + 150, yPosition+150, num[0], num[0]);
+ 
+            }
         }
     }
 
